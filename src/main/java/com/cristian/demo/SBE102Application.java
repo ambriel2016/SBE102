a03b87f8712c9016a75cd7160a1649efee27fd70
GBTC440002UR <GBTC440002UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-17 11:59:08 -0500

    SBE102

--- src/main/java/com/cristian/demo/SBE102Application.java
+++ src/main/java/com/cristian/demo/SBE102Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Exercise102Application {
+public class SBE102Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Exercise102Application.class, args);
+        SpringApplication.run(SBE102Application.class, args);
     }
 
 }
--- src/test/java/com/cristian/demo/SBE102ApplicationTests.java
+++ src/test/java/com/cristian/demo/SBE102ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Exercise102ApplicationTests {
+class SBE102ApplicationTests {
 
     @Test
     void contextLoads() {
